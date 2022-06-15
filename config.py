d655870bafe924524d73f3cd2d886214ea2b4838
Noga Kertes <nogakertes@gmail.com>
2022-06-15 17:00:06 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+"""configuration file for project"""
+
+BATCH_SIZE = 16
+EPHOCHS = 10
+LR = 0.001
+INPUT_CHANNEL_SIZE = 8
+SAVE_NET = True
+PATH_TO_SAVE_NET = 'C:/Users/Nogas/Desktop/Master/mriDL/'
+TRAIN_DATA_PATH = 'C:/Users/Nogas/Desktop/fastmri_data/train_data/'
+VAL_DATA_PATH = 'C:/Users/Nogas/Desktop/fastmri_data/val_data/'
+TEST_DATA_PATH = 'C:/Users/Nogas/Desktop/fastmri_data/val_data/'
