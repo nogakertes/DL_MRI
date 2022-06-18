65362afd7a083ebbe2648612d24970b92e12d136
Noga Kertes <nogakertes@gmail.com>
2022-06-18 14:29:15 +0300

    add normalization transform to kspace before net + use only one type of subsampling masks

--- data_loader.py
+++ data_loader.py
@@ -8,21 +8,20 @@ import fastmri
 
 # Create a mask function
 mask_func = subsample.RandomMaskFunc(
-    center_fractions=[0.08, 0.04],
-    accelerations=[4, 8]
+    center_fractions=[0.04],
+    accelerations=[8]
 )
 def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
     """
     Transform the given k-space data by multiplying with a mask into the subsampled k-space tensor
     We first crop the center of the k-space image to specified height and width, then apply the mask on the cropped image
 
-
     """
     width, height = 320, 320
     kspace = transforms.to_tensor(kspace)
     kspace = transforms.complex_center_crop(kspace, shape=(width, height))
+    kspace,_,_ = transforms.normalize_instance(kspace) #add normalization to kspace
     # # Plot the cropped image for debug
-
     # fig = plt.figure()
     # plt.imshow(np.log(np.abs(kspace[:, :, 0].numpy()) + 1e-9), cmap='gray')     #kspace is complex so choose real or im
     masked_kspace, _,_= transforms.apply_mask(kspace, mask_func)
@@ -33,7 +32,7 @@ def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
     return kspace.reshape((2, width, height)), masked_kspace.reshape((2, width, height))
 
 
-def loadFromDir(dir_path, batch_size, data_type):
+def loadFromDir(dir_path, batch_size, data_type,remove_Edge_slices = None):
     """
     A PyTorch Dataset that provides access to MR image slices.
     """
@@ -58,4 +57,5 @@ def showKspaceFromTensor(tensor):
     image = np.fft.fft2(numpy_kspace)
     plt.subplot(1,2,2)
     plt.imshow(np.abs(np.fft.fftshift(image)), cmap='gray')
+
 
