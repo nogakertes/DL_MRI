45aab8e5d9da9eb6058f92356d93a827698b8ea7
Noga Kertes <nogakertes@gmail.com>
2022-06-25 13:48:47 +0300

    add ssim reg to loss

--- losses.py
+++ losses.py
@@ -35,11 +35,11 @@ def create_window(window_size, channel=1):
 
     return window
 
-def ssim(img1, img2, window_size=11):
+def ssim(img1, img2, DEVICE, window_size=11):
     #L = val_range  # L is the dynamic range of the pixel values (255 for 8-bit grayscale images),
     b,c,h,w = img1.shape
     pad = window_size // 2
-    window = create_window(window_size=11,channel=c)
+    window = create_window(window_size=11,channel=c).to(DEVICE)
     # calculating the mu parameter (locally) for both images using a gaussian filter
     # calculates the luminosity params
     mu1 = F.conv2d(img1, window, padding=pad, groups=c)
@@ -89,9 +89,10 @@ class SSIMLoss(nn.Module):
         self,
         X: torch.Tensor,
         Y: torch.Tensor,
+        DEVICE : str,
         reduced: bool = True,
     ):
-        S = ssim(X,Y)
+        S = ssim(X,Y,DEVICE)
         if reduced:
             return 1 - S.mean()
         else:
